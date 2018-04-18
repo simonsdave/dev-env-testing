1aa44673ea25923fb0b9a3c500c9e9ae35dc2926
Dave Simons <simonsdave@gmail.com>
2018-04-18 11:39:00 -0400

    create_dev_env.sh trusty -> bionic

--- dev_env/create_dev_env.sh
+++ dev_env/create_dev_env.sh
@@ -5,5 +5,5 @@ if [ $# != 4 ]; then
     exit 1
 fi
 
-curl -s https://raw.githubusercontent.com/simonsdave/dev-env/master/ubuntu/trusty/create_dev_env.sh | bash -s -- "$@"
+curl -s https://raw.githubusercontent.com/simonsdave/dev-env/master/ubuntu/bionic/create_dev_env.sh | bash -s -- "$@"
 exit $?
