f79801a7d28a4e11c409bd3707d13e08f7f18bb9
Dave Simons <simonsdave@gmail.com>
2019-03-19 06:03:20 -0400

    refactor: make docker image build reusable

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -5,16 +5,16 @@ set -e
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
 if [ $# != 2 ]; then
-    echo "usage: $(basename "$0") <dist-tar-gz> <image-name>" >&2
+    echo "usage: $(basename "$0") <source-package> <image-name>" >&2
     exit 1
 fi
 
-DIST_TAR_GZ=${1:-}
+SOURCE_PACKAGE=${1:-}
 IMAGE_NAME=${2:-}
 
 CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
 
-cp "$DIST_TAR_GZ" "$CONTEXT_DIR/dev_env_testing.tar.gz"
+cp "$SOURCE_PACKAGE" "$CONTEXT_DIR/source-package.tar.gz"
 docker build -t "$IMAGE_NAME" --file "$SCRIPT_DIR_NAME/Dockerfile" "$CONTEXT_DIR"
 
 rm -rf "$CONTEXT_DIR"
