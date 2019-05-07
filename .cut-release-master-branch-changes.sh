e78862a28c773d1b277be2ef94b96dd52d4a72e0
Dave Simons <simonsdave@gmail.com>
2019-05-06 20:49:19 -0400

    refactor: .cut-release-master-branch-changes.sh

--- .cut-release-master-branch-changes.sh
+++ .cut-release-master-branch-changes.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
-
-#
-# increment the project's version number
-#
-
+ 
 set -e
-
+ 
 if [ $# != 0 ]; then
     echo "usage: $(basename "$0")" >&2
     exit 1
 fi
-
-INIT_DOT_PY=$(repo-root-dir.sh)/$(repo.sh -u)/__init__.py
-CURRENT_VERSION=$(python-version.sh)
-
-# the pip install below is necessary make it super simple
-# to figure out the project's next version
-pip install semantic-version > /dev/null
-
-NEXT_VERSION=$(python -c "import semantic_version; print semantic_version.Version('$CURRENT_VERSION').next_minor()")
-
-sed -i "" -e "s|^[[:space:]]*__version__[[:space:]]*=[[:space:]]*['\"]${CURRENT_VERSION}['\"][[:space:]]*$|__version__ = '${NEXT_VERSION}'|g" "${INIT_DOT_PY}"
-
+ 
+python-increment-version.sh -m
+ 
 exit 0
