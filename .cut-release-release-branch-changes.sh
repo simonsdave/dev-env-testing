9efe37f175d48b9f70ed25e798efe9aff312460c
Dave Simons <simonsdave@gmail.com>
2019-05-06 20:41:40 -0400

    refactor: .cut-release-release-branch-changes.sh

--- .cut-release-release-branch-changes.sh
+++ .cut-release-release-branch-changes.sh
@@ -11,16 +11,16 @@ fi
 
 RELEASE_BRANCH=${1:-}
 
-pushd "$SCRIPT_DIR_NAME"
+pushd "${SCRIPT_DIR_NAME}"
 
 sed -i "" \
-    -e "s|tree/master|tree/$RELEASE_BRANCH|g" \
-    "$SCRIPT_DIR_NAME/README.md"
+    -e "s|tree/master|tree/${RELEASE_BRANCH}|g" \
+    "${SCRIPT_DIR_NAME}/README.md"
 
-rm -f "$SCRIPT_DIR_NAME/README.rst"
+rm -f "${SCRIPT_DIR_NAME}/README.rst"
 build-readme-dot-rst.sh
 
-rm -rf "$SCRIPT_DIR_NAME/dist"
+rm -rf "${SCRIPT_DIR_NAME}/dist"
 build-python-package.sh
 
 popd
